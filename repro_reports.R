9cc5bc2a40298c57105f79fe5d2147229098bcce
Charles Becker <c.l.becker@outlook.com>
2021-03-02 12:00:15 -0800

    Believe.

---
+++ repro_reports.R
@@ -0,0 +1,5 @@
+# HarvardX PH125.5x "Productivity Tools" 2 of 2
+
+library(tidyverse)
+library(dslabs)
+rm(list=ls())
--- warmups.R
+++ warmups.R
@@ -1,7 +1,8 @@
-# HarvardX PH125.5x "Productivity Tools"
+# HarvardX PH125.5x "Productivity Tools" 1 of 2
 
 library(tidyverse)
 library(dslabs)
+rm(list=ls())
 
 MyName <-  readline(prompt="Who are you? ")
 # this str_c function nicely cleans up the mess that is c()
@@ -23,3 +24,12 @@ edit(z_prime_matrix)
 
 # okay, let's get serious here
 print("I am SOOOO serious!")
+
+# practise: generate a random normal distro of m/f heights, plot as kernel density 
+# 10,000 seems to be a sweet spot for getting a nice normal distribution
+m <- rnorm( n = 10000, mean = 70, sd = 3)
+f <- rnorm( n = 10000, mean = 64.5, sd = 2.5)
+mfplot <- ggplot() +
+  geom_density( aes( m ) ) +
+  geom_density( aes( f ) )
+mfplot
